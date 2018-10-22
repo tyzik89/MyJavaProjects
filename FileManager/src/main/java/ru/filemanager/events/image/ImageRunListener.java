2068f01c881d5ceccf9fcc6436dd3f7822848096
Vladimir Sukmanov <33483821+tyzik89@users.noreply.github.com>
2018-10-23 02:27:59 +0300

    update

--- FileManager/src/main/java/ru/filemanager/events/image/ImageRunListener.java
+++ FileManager/src/main/java/ru/filemanager/events/image/ImageRunListener.java
@@ -46,10 +46,15 @@ public class ImageRunListener implements ActionListener {
             ui.getImageLabelRight().setIcon(new ImageIcon(newImage));
         } else if (radioCase.equals("korrelation")) {
             try {
-                ui.setImage(ImageIO.read(new File("src/main/resources/faces.jpg")));
+                ui.setImage(ImageIO.read(new File("src/main/resources/faces.png")));
                 ui.getImageLabelLeft().setIcon(new ImageIcon(ui.getImage()));
                 newImage = new BufferedImage(ui.getImage().getWidth(), ui.getImage().getHeight(), BufferedImage.TYPE_INT_RGB);
-                korrelationAlhoritm();
+
+                BufferedImage faces = ui.getImage();
+                ui.setImage(ImageIO.read(new File("src/main/resources/face.png")));
+                BufferedImage face = ui.getImage();
+
+                korrelationAlhoritm(faces, face);
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
@@ -116,32 +121,132 @@ public class ImageRunListener implements ActionListener {
         }
     }
 
+//    private void korrelationAlhoritm(BufferedImage faces, BufferedImage face) {
+//        int HG = face.getHeight();
+//        int WG = face.getWidth();
+//        int HF = faces.getHeight();
+//        int WF = faces.getWidth();
+//    }
+
+    /*int [][] svertka = {
+                {-1, -1, -1},
+                {-1, 8, -1},
+                {-1, -1, -1}
+        };
+
+        //ВЫДЕЛЕНИЕ ГРАНИЦ
+        for (int  y = 1; y < HG - 1; y++) {
+            for (int x = 1; x < WG - 1; x++) {
+                sum = 0;
+                for (int j = 0; j < 3; j++) {
+                    for (int i = 0; i < 3; i++) {
+                        pixelColorG = new Color(face.getRGB(x - 1 + i, y - 1 + j));
+                        brightnessPixelG = (pixelColorG.getRed() + pixelColorG.getBlue() + pixelColorG.getGreen()) / 3;
+                        sum += brightnessPixelG * svertka[j][i];
+                    }
+                }
+                if (sum < 0)
+                    sum = 0;
+                if (sum > 255)
+                    sum = 255;
+                Color color = new Color((int)sum, (int)sum, (int)sum);
+                newImage.setRGB(x, y, color.getRGB());
+            }
+        }*/
+
     /**
      * Алгоритм распознавания избражения
      */
-    private void korrelationAlhoritm() {
-        /*% h = f(260:475,725:940);
-            h = f(500:700,725:940);*/
-        BufferedImage faces = ui.getImage();
-        BufferedImage face = faces.getSubimage(750, 500, 150, 150);
-        //Алгоритм
+    private void korrelationAlhoritm(BufferedImage faces, BufferedImage face) {
         int HG = face.getHeight();
         int WG = face.getWidth();
         int HF = faces.getHeight();
         int WF = faces.getWidth();
 
-        for (int y = HG / 2; y < HF - (HG / 2) ; y++) {
-            for (int x = WG / 2; x < WF - (WG / 2) ; x++) {
-                int sum = 0;
-                for (int j = 0; j < HG; j++) {
-                    for (int i = 0; i < WG; i++) {
-                        int pixelG = face.getRGB(i, j);
-                        int pixelF = faces.getRGB(x - (WG / 2) + i, y - (HG / 2) + j);
-                        sum += pixelG * pixelF;
+        float sum = 0;
+        int max = -1;
+        Color pixelColorG;
+        Color pixelColorF;
+        float brightnessPixelG;
+        float brightnessPixelF;
+
+        BufferedImage faceBorder= new BufferedImage(face.getWidth(), face.getHeight(), BufferedImage.TYPE_INT_RGB);
+        BufferedImage facesBorder= new BufferedImage(faces.getWidth(), faces.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        int [][] svertka = {
+                {-1, -1, -1},
+                {-1, 8, -1},
+                {-1, -1, -1}
+        };
+        //ВЫДЕЛЕНИЕ ГРАНИЦ У ШАБЛОНА
+        for (int  y = 1; y < HG - 1; y++) {
+            for (int x = 1; x < WG - 1; x++) {
+                sum = 0;
+                for (int j = 0; j < 3; j++) {
+                    for (int i = 0; i < 3; i++) {
+                        pixelColorG = new Color(face.getRGB(x - 1 + i, y - 1 + j));
+                        brightnessPixelG = (pixelColorG.getRed() + pixelColorG.getBlue() + pixelColorG.getGreen()) / 3;
+                        sum += brightnessPixelG * svertka[j][i];
                     }
                 }
-                newImage.setRGB(x, y, (int)sum);
+                if (sum < 0)
+                    sum = 0;
+                if (sum > 255)
+                    sum = 255;
+                if (max < sum)
+                    max = (int)sum;
+                Color color = new Color((int)sum, (int)sum, (int)sum);
+                faceBorder.setRGB(x, y, color.getRGB());
             }
         }
+
+
+
+        //ВЫДЕЛЕНИЕ ГРАНИЦ У ИСХОДНИКА
+        for (int  y = 1; y < HF - 1; y++) {
+            for (int x = 1; x < WF - 1; x++) {
+                sum = 0;
+                for (int j = 0; j < 3; j++) {
+                    for (int i = 0; i < 3; i++) {
+                        pixelColorF = new Color(faces.getRGB(x - 1 + i, y - 1 + j));
+                        brightnessPixelF = (pixelColorF.getRed() + pixelColorF.getBlue() + pixelColorF.getGreen()) / 3;
+                        sum += brightnessPixelF * svertka[j][i];
+                    }
+                }
+                if (sum < 0)
+                    sum = 0;
+                if (sum > 255)
+                    sum = 255;
+                if (max < sum)
+                    max = (int)sum;
+                Color color = new Color((int)sum, (int)sum, (int)sum);
+                facesBorder.setRGB(x, y, color.getRGB());
+            }
+        }
+
+
+        max = -1;
+        BufferedImage result= new BufferedImage(faces.getWidth(), faces.getHeight(), BufferedImage.TYPE_INT_RGB);
+        for (int y = HG / 2; y < HF - (HG / 2) ; y++) {
+            for (int x = WG / 2; x < WF - (WG / 2); x++) {
+                sum = 0;
+                for (int j = 0; j < HG; j++) {
+                    for (int i = 0; i < WG; i++) {
+                        pixelColorG = new Color(faceBorder.getRGB(i , j));
+                        pixelColorF = new Color(facesBorder.getRGB( x - (WG / 2) + i, y - (HG / 2) + j));
+                        //Яркость каждого пикселя
+                        brightnessPixelG = (pixelColorG.getRed() + pixelColorG.getBlue() + pixelColorG.getGreen()) / 3;
+                        brightnessPixelF = (pixelColorF.getRed() + pixelColorF.getBlue() + pixelColorF.getGreen()) / 3;
+                        sum += brightnessPixelG * brightnessPixelF;
+                    }
+                }
+                sum = 255 / sum;
+                Color color = new Color((int)sum, (int)sum, (int)sum);
+                result.setRGB(x, y, color.getRGB());
+                //System.out.println();
+            }
+        }
+        newImage = result;
+
     }
 }
