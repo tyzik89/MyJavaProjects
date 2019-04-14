b40f0e9b35839470ad4caed418812deed05dd97b
Vladimir Sukmanov <33483821+tyzik89@users.noreply.github.com>
2019-04-14 17:23:57 +0300

    Таблица умножения

---
+++ Lessons/src/com/work/vladimirs/multiplication_table/MultiplicationTable.java
@@ -0,0 +1,31 @@
+package com.work.vladimirs.multiplication_table;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Считываем 4-ре числа
+        int a = scanner.nextInt();
+        scanner.nextLine();
+        int b = scanner.nextInt();
+        scanner.nextLine();
+        int c = scanner.nextInt();
+        scanner.nextLine();
+        int d = scanner.nextInt();
+
+        for (int k = c; k <= d; k++ )
+            System.out.printf("\t%d", k);
+        System.out.println();
+
+        for (int i = a; i <= b; i++) {
+            System.out.printf("%d\t", i);
+            for(int j = c; j <= d; j++) {
+                System.out.printf("%d\t", i*j);
+            }
+            System.out.println();
+        }
+    }
+}
