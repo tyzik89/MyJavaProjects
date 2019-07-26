b861d4519329fbff76654b6e9fceecb8c9c689b0
Vladimir <work.vladimir.s@gmail.com>
2019-07-26 12:38:09 +0300

    GET/POST Requests

--- HttpRequests(GET, POST)/src/com/work/vladimirs/ApacheHttpClientExample.java
+++ HttpRequests(GET, POST)/src/com/work/vladimirs/ApacheHttpClientExample.java
@@ -1,9 +1,25 @@
 package com.work.vladimirs;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApacheHttpClientExample {
     private final String USER_AGENT = "Mozilla/5.0";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ApacheHttpClientExample http = new ApacheHttpClientExample();
 
         System.out.println("Testing 1 - Send Http GET request");
@@ -14,10 +30,11 @@ public class ApacheHttpClientExample {
     }
 
     // HTTP GET request
-    private void sendGet() {
+    private void sendGet() throws IOException {
         String url = "http://www.google.com/search?q=developer";
 
-        HttpClient client = new DefaultHttpClient();
+        //HttpClient client = new DefaultHttpClient();
+        HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet(url);
 
         // add request header
@@ -42,7 +59,40 @@ public class ApacheHttpClientExample {
     }
 
     // HTTP POST request
-    private void sendPost() {
+    private void sendPost() throws IOException {
+        String url = "https://selfsolve.apple.com/wcResults.do";
 
+        //HttpClient client = new DefaultHttpClient();
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(url);
+
+        // add header
+        post.setHeader("User-Agent", USER_AGENT);
+
+        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+        urlParameters.add(new BasicNameValuePair("sn", "C02G8416DRJM"));
+        urlParameters.add(new BasicNameValuePair("cn", ""));
+        urlParameters.add(new BasicNameValuePair("locale", ""));
+        urlParameters.add(new BasicNameValuePair("caller", ""));
+        urlParameters.add(new BasicNameValuePair("num", "12345"));
+
+        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+        HttpResponse response = client.execute(post);
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + post.getEntity());
+        System.out.println("Response Code : " +
+                response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+
+        System.out.println(result.toString());
     }
 }
