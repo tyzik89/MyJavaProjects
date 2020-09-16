084ca6ce7ef7f1326ba87f04c04559f020c949ee
work.vladimir.s@gmail.com <Corpse88github>
2020-09-16 11:31:24 +0300

    Копирование файлов в одну сторону

---
+++ SyncF/src/com/work/vladimirs/CustomFileVisitor.java
@@ -0,0 +1,55 @@
+package com.work.vladimirs;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
+
+public class CustomFileVisitor implements FileVisitor<Path> {
+    private HashSet<Path> pathHashSet;
+    private Path startPath;
+    private String stringStartPath;
+
+    public CustomFileVisitor(HashSet<Path> pathHashSet, Path startPath) {
+        this.pathHashSet = pathHashSet;
+        this.startPath = startPath;
+        stringStartPath = startPath.toString().replace("\\", "\\\\");
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        Path newPath = getPathForRegexp(dir);
+        if (!newPath.toString().isEmpty()) pathHashSet.add(newPath);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Path newPath = getPathForRegexp(file);
+        if (!newPath.toString().isEmpty()) pathHashSet.add(newPath);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    public HashSet<Path> getPathHashSet() {
+        return pathHashSet;
+    }
+
+    private Path getPathForRegexp(Path pathWithRoot) {
+        String stringPathWithRoot = pathWithRoot.toString();
+        String stringClearPath = stringPathWithRoot.replaceFirst(stringStartPath, "");    // Выделяем из пути часть, не содержащую сам корневой путь.
+        return Paths.get(stringClearPath);
+    }
+}
--- SyncF/src/com/work/vladimirs/Main.java
+++ SyncF/src/com/work/vladimirs/Main.java
@@ -3,27 +3,41 @@ package com.work.vladimirs;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    public static final String REMOTE_DIR= "E://Тату";
-    public static final String LOCAL_DIR= "C://Тату";
+    public static final String REMOTE_DIR = "D:/Повышение";
+    public static final String LOCAL_DIR = "C:/Повышение";
 
 
     public static void main(String[] args) throws IOException {
         File f1 = new File(REMOTE_DIR);
         File f2 = new File(LOCAL_DIR);
 
-        File[] files = f1.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
+        Path startRemotePath = Paths.get(REMOTE_DIR);
+        Path startLocalPath = Paths.get(LOCAL_DIR);
 
-            } else if(file.isFile()) {
-                //Files.copy(Paths.get(file.getPath()), f2.toPath());
-                System.out.println(file.getPath());
-                Files.copy(file.toPath(), Paths.get(f2.getPath() + "\\" + file.getName()));
+        CustomFileVisitor remoteFileVisitor = new CustomFileVisitor(new HashSet<>(), startRemotePath);
+        Files.walkFileTree(Paths.get(REMOTE_DIR), Collections.EMPTY_SET, Integer.MAX_VALUE, remoteFileVisitor);
+        Set<Path> remotePaths = remoteFileVisitor.getPathHashSet();
+
+        CustomFileVisitor localFileVisitor = new CustomFileVisitor(new HashSet<>(), startLocalPath);
+        Files.walkFileTree(Paths.get(LOCAL_DIR), Collections.EMPTY_SET, Integer.MAX_VALUE, localFileVisitor);
+        Set<Path> localPaths = localFileVisitor.getPathHashSet();
+
+        remotePaths.removeAll(localPaths);
+        remotePaths.forEach(System.out::println);
+        remotePaths.forEach(path -> {
+            try {
+                Files.copy(Paths.get(REMOTE_DIR + path), Paths.get(LOCAL_DIR + path));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
+        });
+
     }
 }
