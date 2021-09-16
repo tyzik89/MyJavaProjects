de5d6aa6ba473674ea1a76a5fb1811311de581a8
work.vladimir.s@gmail.com <Corpse88github>
2021-09-16 16:04:57 +0300

    Java Substring Comparisons ver.2

--- Lessons/src/com/work/vladimirs/lexicographical_substrings_comparisions/LexicographicalCalculator.java
+++ Lessons/src/com/work/vladimirs/lexicographical_substrings_comparisions/LexicographicalCalculator.java
@@ -13,31 +13,16 @@ public class LexicographicalCalculator {
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = "";
         String largest = "";
-        HashMap<String, List<String>> dictionary = new HashMap<>();
 
-        String[] charOfString = s.split("");
-        StringBuilder builder = new StringBuilder(k);
-        for (int i = 0; i < charOfString.length - 1; i++) {
-            if ((i + k) <= charOfString.length) {
-                builder.append(charOfString[i]);
-                for (int j = 1; j < k; j++) {
-                    builder.append(charOfString[i + j]);
-                }
-                dictionary.compute(charOfString[i], (key, value) -> value == null ? new ArrayList<>() : value).add(builder.toString());
-                builder.setLength(0);
-            }
+        smallest = s.substring(0, k);
+        largest = s.substring(0, k);
+        for (int i = 1; i < s.length() - 1; i++) {
+            k += 1;
+            if (k > s.length()) break;
+            String temp = s.substring(i, k);
+            smallest = smallest.compareTo(temp) > 0 ? temp : smallest;
+            largest = largest.compareTo(temp) < 0 ? temp : largest;
         }
-
-        for (Map.Entry<String, List<String>> entry : dictionary.entrySet()) {
-            System.out.printf("Key: %s  Value: %s \n", entry.getKey(), entry.getValue());
-        }
-
-        Object[] keys = dictionary.keySet().stream().sorted().toArray();
-        List<String> smallestList = dictionary.get(keys[0]);
-        List<String> largestList = dictionary.get(keys[keys.length - 1]);
-
-        smallest = smallestList.stream().sorted().findFirst().get();
-        largest = largestList.stream().min(Comparator.reverseOrder()).get();
 
         return smallest + "\n" + largest;
     }
