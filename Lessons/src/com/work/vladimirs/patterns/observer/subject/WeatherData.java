368e04e923f7117c016fc41a434e68bfc7c6faa5
work.vladimir.s@gmail.com <Corpse88github>
2021-11-03 15:18:53 +0300

    Паттерн наблюдатель

--- Lessons/src/com/work/vladimirs/patterns/observer/subject/WeatherData.java
+++ Lessons/src/com/work/vladimirs/patterns/observer/subject/WeatherData.java
@@ -2,21 +2,34 @@ package com.work.vladimirs.patterns.observer.subject;
 
 import com.work.vladimirs.patterns.observer.observers.Observer;
 
+import java.util.ArrayList;
+
 public class WeatherData implements Subject {
+
+    private ArrayList<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        observers = new ArrayList<Observer>();
+    }
 
     @Override
     public void registerObserver(Observer o) {
-
+        observers.add(o);
     }
 
     @Override
     public void removeObserver(Observer o) {
-
+        observers.remove(o);
     }
 
     @Override
     public void notifyObserver() {
-
+        for (Observer observer : observers) {
+            observer.update(temperature, humidity, pressure);
+        }
     }
 
     /**
@@ -35,12 +48,22 @@ public class WeatherData implements Subject {
     public void getPressure() {}
 
     /**
-     * Обновление изображения для трёх элементов:
+     * Оповещение наблюдаталей о появлении новых данных:
      * - текущее состояние;
      * - статистика;
      * - прогноз;
      */
     public void measurementsChanged() {
+        notifyObserver();
+    }
 
+    /**
+     * Чтение данных с метеостанции
+     */
+    public void setMeasurements(float temp, float humidity, float pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
     }
 }
