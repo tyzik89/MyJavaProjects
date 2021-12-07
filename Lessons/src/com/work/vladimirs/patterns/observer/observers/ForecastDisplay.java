9a5fea4efb33eeda9336ee86925306395ce615f8
work.vladimir.s@gmail.com <Corpse88github>
2021-12-07 13:16:20 +0300

    Паттерн наблюдатель

--- Lessons/src/com/work/vladimirs/patterns/observer/observers/ForecastDisplay.java
+++ Lessons/src/com/work/vladimirs/patterns/observer/observers/ForecastDisplay.java
@@ -1,9 +1,20 @@
 package com.work.vladimirs.patterns.observer.observers;
 
+import com.work.vladimirs.patterns.observer.subject.WeatherData;
+
 /**
  * Элемент дисплея, выводящий прогноз погоды по показаниям барометра
  */
 public class ForecastDisplay implements Observer, DisplayElement {
+
+    private WeatherData weatherData;
+    private float currentPressure;
+    private float lastPressure;
+
+    public ForecastDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
 
     @Override
     public void display() {
