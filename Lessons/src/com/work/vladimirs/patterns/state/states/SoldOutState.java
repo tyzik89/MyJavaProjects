e09ed824a1db8349b2900100a68169f6fd8f83ef
work.vladimir.s@gmail.com <Corpse88github>
2022-07-05 18:32:23 +0300

    Паттерн состояние

--- Lessons/src/com/work/vladimirs/patterns/state/states/SoldOutState.java
+++ Lessons/src/com/work/vladimirs/patterns/state/states/SoldOutState.java
@@ -15,21 +15,21 @@ public class SoldOutState implements State {
 
     @Override
     public void insertQuarter() {
-
+        System.out.println("You cannot insert a quarter, because all gumballs was sold.");
     }
 
     @Override
     public void ejectQuarter() {
-
+        System.out.println("You haven't inserted a quarter, and all gumballs was sold.");
     }
 
     @Override
     public void turnCrank() {
-
+        System.out.println("You turned, but there’s no quarter and all gumballs was sold.");
     }
 
     @Override
     public void dispense() {
-
+        System.out.println("You need to pay first, but all gumballs was sold.");
     }
 }
