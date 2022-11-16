1cb5bf88bf8bd741d3ab5a46f0b37cce1173a20e
Vladimir <work.vladimir.s@gmail.com>
2022-11-17 00:24:20 +0300

    Controllers and layouts

--- PhoneticTextAnalysis/src/main/java/controllers/RootLayoutController.java
+++ PhoneticTextAnalysis/src/main/java/controllers/RootLayoutController.java
@@ -3,20 +3,13 @@ package controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
-import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 
 public class RootLayoutController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(RootLayoutController.class);
-
-    private final FXMLLoader loader = new FXMLLoader();
 
     @FXML
     private MenuItem itmLoad;
@@ -30,15 +23,7 @@ public class RootLayoutController {
 
     @FXML
     public void handleWordLoad(ActionEvent actionEvent) {
-        loader.setLocation(getClass().getResource("/fxml/WordLoadLayout.fxml"));
-        try {
-            Parent parent = loader.load();
-            Stage stage = new Stage();
-            stage.setTitle("Анализ слова");
-            stage.setScene(new Scene(parent));
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        WordLoadController controller = new WordLoadController();
+        controller.init();
     }
 }
--- PhoneticTextAnalysis/src/main/java/controllers/WordLoadController.java
+++ PhoneticTextAnalysis/src/main/java/controllers/WordLoadController.java
@@ -2,14 +2,26 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
 
 public class WordLoadController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(WordLoadController.class);
+
+    private static Stage wordLoadStage;
+
+    private CharSequence charSequence;
 
     @FXML
     public Button btnLoad;
@@ -17,14 +29,49 @@ public class WordLoadController {
     public Button itmExit;
     @FXML
     public TextField itmWordInput;
+    @FXML
+    public Label errorLabel;
+
+    public void init() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/WordLoadLayout.fxml"));
+            Parent parent = loader.load();
+            wordLoadStage = new Stage();
+            wordLoadStage.setTitle("Анализ слова");
+            wordLoadStage.setScene(new Scene(parent));
+            wordLoadStage.setResizable(false);
+            wordLoadStage.setMaximized(false);
+            wordLoadStage.setAlwaysOnTop(true);
+            wordLoadStage.initModality(Modality.APPLICATION_MODAL);
+            wordLoadStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     public void handleAnalyze(ActionEvent actionEvent) {
-
+        CharSequence charSequence = itmWordInput.getCharacters();
+        if (charSequence.length() > 0) {
+            LOGGER.debug(charSequence.toString());
+            errorLabel.setText("");
+            this.charSequence = charSequence;
+            wordLoadStage.hide();
+        } else {
+            errorLabel.setText("Пожалуйста, введите слово:");
+        }
     }
 
     @FXML
     public void handleExit(ActionEvent actionEvent) {
+        wordLoadStage.hide();
+    }
 
+    public CharSequence getCharSequence() {
+        return charSequence;
+    }
+
+    public void setCharSequence(CharSequence charSequence) {
+        this.charSequence = charSequence;
     }
 }
