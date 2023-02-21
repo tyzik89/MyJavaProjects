72115f632a654621bf1614d392f8e5092bd3e582
work.vladimir.s@gmail.com <Corpse88github>
2023-02-21 17:24:18 +0300

    Регулярные выражения. Убрать дубликаты слов в строке

---
+++ Lessons/src/com/work/vladimirs/regex/duplicate_words/Solution.java
@@ -0,0 +1,36 @@
+package com.work.vladimirs.regex.duplicate_words;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String regex = "(\\b\\w+\\b).*(\\1)";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        List<String> strings = new ArrayList<String>(){{
+            add("Goodbye bye bye world world world");
+            add("Sam went went to to to his business");
+            add("Reya is is the the best player in eye eye game");
+            add("in inthe");
+            add("Hello hello Ab aB");
+        }};
+
+        for (String input : strings) {
+            System.out.println(input);
+            Matcher m = p.matcher(input);
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                System.out.println("m.group(): " + m.group() + ";m.group(1): " + m.group(1) + "; m.group(2):" + m.group(2));
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+            // Prints the modified sentence.
+            System.out.println(input + "\n");
+        }
+    }
+
+}
