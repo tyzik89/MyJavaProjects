b60d2fe5928f1dc8178a18fc14a99f0b2cf9fdf9
work.vladimir.s@gmail.com <Corpse88github>
2023-06-29 15:21:33 +0300

    Deque

--- Lessons/src/com/work/vladimirs/other/dequeue/DequeTest.java
+++ Lessons/src/com/work/vladimirs/other/dequeue/DequeTest.java
@@ -1,9 +1,7 @@
 package com.work.vladimirs.other.dequeue;
 
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.*;
 
 public class DequeTest {
 
@@ -13,14 +11,19 @@ public class DequeTest {
         int n = in.nextInt();
         int m = in.nextInt();
         long maxUniqNum = 0;
+        Set<Integer> integerSet = new HashSet<>();
         for (int i = 0; i < n; i++) {
             int num = in.nextInt();
             deque.addLast(num);
+            integerSet.add(num);
             if (deque.size() == m) {
-//                int currentSize = new HashSet<>(deque).size();
-                long currentSize = deque.stream().distinct().count();
-                maxUniqNum = Math.max(currentSize, maxUniqNum);
-                deque.removeFirst();
+//                int currentSize = new HashSet<>(deque).size(); // too slow
+//                long currentSize = deque.stream().distinct().count(); // too slow
+                maxUniqNum = Math.max(integerSet.size(), maxUniqNum);
+                Integer removedElem = deque.removeFirst();
+                if (!deque.contains(removedElem)) {
+                    integerSet.remove(removedElem);
+                }
             }
         }
 
