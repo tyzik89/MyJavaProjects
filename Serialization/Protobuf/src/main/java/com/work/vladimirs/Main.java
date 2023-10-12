d78db6bf609e87148fc61985745d839c1b4c9df8
work.vladimir.s@gmail.com <Corpse88github>
2023-10-12 16:38:16 +0300

    Protobuf

--- Serialization/Protobuf/src/main/java/com/work/vladimirs/Main.java
+++ Serialization/Protobuf/src/main/java/com/work/vladimirs/Main.java
@@ -1,7 +1,24 @@
 package com.work.vladimirs;
 
+import com.work.vladimirs.proto.Person;
+
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String email = "j@gmail.com";
+        int id = new Random().nextInt();
+        String name = "Vovka Program";
+        String number = "01234567890";
+        Person person =
+                Person.newBuilder()
+                        .setId(id)
+                        .setName(name)
+                        .setEmail(email)
+                        .addNumbers(number)
+                        .build();
+
+        System.out.println(person);
     }
 }
